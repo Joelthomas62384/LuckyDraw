3230ab014ba0b263ffcbaaf5aaf639b8bdaccbbd
Joelthomas62384 <103513948+Joelthomas62384@users.noreply.github.com>
2024-06-07 20:04:54 +0530

    Update NavLinks.jsx new

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -24,16 +24,16 @@ const NavLinks = () => {
             <div className="w-full flex justify-center items-center">
                 <ul className="flex flex-wrap justify-between text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
                     <li className="me-2">
-                        <Link href="/" onClick={() => handleActive('/LuckyDraw/')} aria-current={active === '/LuckyDraw/' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/LuckyDraw/' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Add Names</Link>
+                        <Link href="/" onClick={() => handleActive('/')} aria-current={active === '/' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Add Names</Link>
                     </li>
                     <li className="me-2">
-                        <Link href="/names" onClick={() => handleActive('/LuckyDraw/names')} aria-current={active === '/LuckyDraw/names' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/LuckyDraw/names' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Names</Link>
+                        <Link href="/names" onClick={() => handleActive('/names')} aria-current={active === '/names' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/names' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Names</Link>
                     </li>
                     <li className="me-2">
-                        <Link href="/lucky-draw" onClick={() => handleActive('/LuckyDraw/lucky-draw')} aria-current={active === '/LuckyDraw/lucky-draw' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/LuckyDraw/lucky-draw' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Lucky Draw</Link>
+                        <Link href="/lucky-draw" onClick={() => handleActive('/lucky-draw')} aria-current={active === '/lucky-draw' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/lucky-draw' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Lucky Draw</Link>
                     </li>
                     <li className="me-2">
-                        <Link href="/winners" onClick={() => handleActive('/LuckyDraw/winners')} aria-current={active === '/LuckyDraw/winners' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/LuckyDraw/winners' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Winners</Link>
+                        <Link href="/winners" onClick={() => handleActive('/winners')} aria-current={active === '/winners' ? 'page' : undefined} className={`inline-block p-1 md:p-4 ${active === '/winners' ? 'text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500' : 'text-gray-500'}`}>Winners</Link>
                     </li>
                 </ul>
             </div>
