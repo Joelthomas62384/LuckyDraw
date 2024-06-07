01765c3358a31fcfe3c4ec16ed76865cb1184b46
Joelthomas62384 <103513948+Joelthomas62384@users.noreply.github.com>
2024-06-07 20:09:53 +0530

    Update page.jsx

--- src/app/winners/page.jsx
+++ src/app/winners/page.jsx
@@ -11,7 +11,7 @@ const Winners = () => {
 
   return (
     <div className="w-full md:w-[50%] mx-auto my-10">
-      <div className="relative overflow-x-hidden overflow-auto h-[50vh] shadow-md sm:rounded-lg">
+      <div className="relative overflow-x-hidden overflow-auto h-[30vh] shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
